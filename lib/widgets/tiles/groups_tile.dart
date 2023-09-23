9cc0fc909b93b9063707c098ab7e75dd831881b9
tvn24van <wojciech.smaza@gmail.com>
2023-09-23 07:16:28 +0200

    groups_tile cleanup

--- lib/widgets/tiles/groups_tile.dart
+++ lib/widgets/tiles/groups_tile.dart
@@ -12,7 +12,6 @@ class PansGroupsTile extends ConsumerWidget{
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final groups = ref.watch(SettingsProvider.instance.select((settings) => settings.groups));
-    // final schedule = ref.watch(ScheduleProvider.instance);
     final teacherMode = ref.watch(SettingsProvider.instance.select((settings) => settings.isTeacher));
     final isSpecializationSelected = ref.watch(
       SettingsProvider.instance.select((settings) => settings.specializationKey)
@@ -24,7 +23,6 @@ class PansGroupsTile extends ConsumerWidget{
 
     return ref.watch(ScheduleProvider.instance).when(
       data:(data){
-        // final maxGroup = data.getMaxGroupNumber();
 
         return Column(
           mainAxisSize: MainAxisSize.min,
@@ -34,41 +32,22 @@ class PansGroupsTile extends ConsumerWidget{
               return const SizedBox.shrink();
             }
             return ListTile(
-              title: Text("${L10n
-                  .of(context)
-                  .group} ${group.symbol} (${L10n.of(context).group_name(
-                  group.name)})"),
+              title: Text("${L10n.of(context).group} ${group.symbol} (${L10n.of(context).group_name(group.name)})"),
               trailing: SizedBox(
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width * .6,
+                width: MediaQuery.of(context).size.width * .6,
                 child: SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
-                  // child: ToggleButtons(
-                  //   isSelected: List.generate(maxGroup, (index)=>
-                  //     groups[group]!.contains(index+1)
-                  //   ),
-                  //   children: List.generate(maxGroup, (index)=> Text("${index+1}")),
-                  //   onPressed: (index)=>{
-                  //     ref.read(SettingsProvider.instance.notifier).toggleGroupNumber(
-                  //       group,
-                  //       index+1
-                  //     )
-                  //   },
-                  // ),
                   child: SegmentedButton<int>(
                     emptySelectionAllowed: true,
                     showSelectedIcon: false,
                     multiSelectionEnabled: true,
                     selected: groups[group]!,
                     segments: List.generate(maxGroups, (
-                        index) =>
-                        ButtonSegment(
-                            value: index + 1,
-                            // enabled: !groups[group]!.contains(index+1),
-                            label: Text("${index + 1}")
-                        )
+                      index) =>
+                      ButtonSegment(
+                        value: index + 1,
+                        label: Text("${index + 1}")
+                      )
                     ),
                     onSelectionChanged: (selection) {
                       ref.read(SettingsProvider.instance.notifier)
