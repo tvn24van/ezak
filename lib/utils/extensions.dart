022b7013adaa6278c225c8c34079031689a4ad41
tvn24van <wojciech.smaza@gmail.com>
2024-10-02 11:08:48 +0200

    these things are subject to removal

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -35,23 +35,23 @@ extension TimeOfDayExtension on TimeOfDay{
   operator >(TimeOfDay other) => minutes > other.minutes;
 }
 
-extension DurationToHour on Duration{
-  String formatTime(){
-    final whole = '$this'.split('.')[0].padLeft(8, '0');
-    return whole.substring(0, whole.length-3);
-  }
-}
-
-extension JsonToCoursesList on Iterable{
-  CoursesList toCoursesList(){
-    return List<Course>.from(map((model) => Course.fromJson(model)));
-  }
-}
-
-extension JsonToDatesMap on Iterable{
-  DatesMap toDatesMap(){
-    return fold({}, (map, item) =>
-      map..putIfAbsent(DateTime.parse(item['dzien']), () => []).add(item['pk'])
-    );
-  }
-}
+// extension DurationToHour on Duration{
+//   String formatTime(){
+//     final whole = '$this'.split('.')[0].padLeft(8, '0');
+//     return whole.substring(0, whole.length-3);
+//   }
+// }
+//
+// extension JsonToCoursesList on Iterable{
+//   CoursesList toCoursesList(){
+//     return List<Course>.from(map((model) => Course.fromJson(model)));
+//   }
+// }
+//
+// extension JsonToDatesMap on Iterable{
+//   DatesMap toDatesMap(){
+//     return fold({}, (map, item) =>
+//       map..putIfAbsent(DateTime.parse(item['dzien']), () => []).add(item['pk'])
+//     );
+//   }
+// }
