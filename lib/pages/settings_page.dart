5e33a4d3bf4e73d45ed87883ee36b3e99ee0dcb7
tvn24van <wojciech.smaza@gmail.com>
2024-10-05 17:57:32 +0200

    throw out feature_discovery

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,7 @@
 import 'package:ezak/model/settings.dart';
 import 'package:ezak/pages/schedule_page.dart';
 import 'package:ezak/providers/settings_provider.dart';
-import 'package:ezak/utils/l10n/l10n.g.dart';
+import 'package:ezak/l10n/l10n.g.dart';
 import 'package:ezak/widgets/app_bar.dart';
 import 'package:ezak/widgets/tiles/auto_update_tile.dart';
 import 'package:ezak/widgets/tiles/clear_data_tile.dart';
@@ -12,7 +12,6 @@ import 'package:ezak/widgets/popup_items/about_popup_item.dart';
 import 'package:ezak/widgets/tiles/specialization_tile.dart';
 import 'package:ezak/widgets/tiles/language_tile.dart';
 import 'package:ezak/widgets/tiles/teacher_tile.dart';
-import 'package:feature_discovery_fork/feature_discovery.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,62 +21,50 @@ final class SettingsPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return FeatureDiscovery(
-      recordStepsInSharedPreferences: false,
-      child: Scaffold(
-        appBar: PansAppBar(
-          pansLeading: Tooltip(
-            message: MaterialLocalizations.of(context).backButtonTooltip,
-            child: Consumer(
-              builder:(context, ref, child){
-                final firstLaunch = ref.watch(
-                  SettingsProvider.instance.select((settings) => settings.specializationKey)
-                ) == Settings.defaultSpecializationKey;
+    return Scaffold(
+      appBar: PansAppBar(
+        pansLeading: Tooltip(
+          message: MaterialLocalizations.of(context).backButtonTooltip,
+          child: Consumer(
+            builder:(context, ref, child){
+              final firstLaunch = ref.watch(
+                SettingsProvider.instance.select((settings) => settings.specializationKey)
+              ) == Settings.defaultSpecializationKey;
 
-                final iconButton = IconButton(
-                  onPressed: firstLaunch? null : (){
-                    if(Navigator.canPop(context)) {
-                      Navigator.of(context).pop();
-                    }else{
-                      Navigator.pushReplacement(
+              return IconButton(
+                onPressed: firstLaunch? null : (){
+                  if(Navigator.canPop(context)) {
+                    Navigator.of(context).pop();
+                  }else{
+                    Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(builder: (_) => const SchedulePage())
-                      );
-                    }
-                  },
-                  icon: Icon(Icons.adaptive.arrow_back)
-                );
-
-                return DescribedFeatureOverlay(
-                  featureId: "go_to_schedule",
-                  title: Text(L10n.of(context).go_to_schedule),
-                  backgroundColor: Colors.green,
-                  targetColor: Theme.of(context).canvasColor,
-                  tapTarget: iconButton,
-                  child: iconButton,
-                );
-              },
-            ),
+                    );
+                  }
+                },
+                icon: Icon(Icons.adaptive.arrow_back)
+              );
+            },
           ),
-          leadingText: L10n.of(context).settings,
-          popupItems: [
-            PansAboutAppPopupItem(context)
-          ],
-          context: context,
         ),
-        body: const SingleChildScrollView(
-          child: Column(
-            children: [
-              PansLanguageTile(),
-              PansThemeTile(),
-              PansAutoUpdateTile(),
-              PansTeacherTile(),
-              PansSpecializationTile(),
-              PansGroupsTile(),
-              PansUpdateScheduleTile(),
-              PansClearDataTile()
-            ],
-          ),
+        leadingText: L10n.of(context).settings,
+        popupItems: [
+          PansAboutAppPopupItem(context)
+        ],
+        context: context,
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            PansLanguageTile(),
+            PansThemeTile(),
+            PansAutoUpdateTile(),
+            PansTeacherTile(),
+            PansSpecializationTile(),
+            PansGroupsTile(),
+            PansUpdateScheduleTile(),
+            PansClearDataTile()
+          ],
         ),
       ),
     );
