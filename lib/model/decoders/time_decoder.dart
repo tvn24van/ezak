3d19d31faeace7fc1d8b5fbd225f2b6c2d4ea99b
tvn24van <wojciech.smaza@gmail.com>
2024-10-12 00:24:26 +0200

    getting rid of another method and commented previous implementation

--- lib/model/decoders/time_decoder.dart
+++ lib/model/decoders/time_decoder.dart
@@ -1,37 +1,10 @@
-// import 'package:time/time.dart';
-//
-// // use TimeOfDay
-//
-// /// Util for decoding raw [int]s provided
-// /// by rest api to more usable [Duration]
-// final class HoursDecoder{
-//   /// Break time between double length standard courses
-//   static final Duration breakLength = 10.minutes;
-//   /// Time of single course (standard course takes double this time)
-//   static final Duration courseTime = 45.minutes;
-//   /// Literally 7:15 (.25 in declaration is just a quarter of hour in minutes)
-//   static final Duration startTime = 7.25.hours;
-//
-//   /// Uses start hour [code] provided by rest api
-//   /// to generate time in [Duration]
-//   static Duration decodeStartHour(int code){
-//     final breakLengthSum = breakLength * (code ~/ 2) - (code<2? Duration.zero : breakLength);
-//     return startTime + courseTime * (code-1) + breakLengthSum;
-//   }
-//
-//   /// Uses [code] which indicates a start hour
-//   /// and [lengthCode] which is length of [Course]
-//   static Duration decodeEndHour(int code, int lengthCode){
-//     final sum = code + lengthCode;
-//     return decodeStartHour(sum) - (sum%2!=0? Duration.zero : breakLength);
-//   }
-// }
-
+import 'package:ezak/utils/extensions.dart';
 import 'package:flutter/material.dart';
 
 /// Util for decoding raw [int]s provided
 /// by rest api to more usable [TimeOfDay]
 final class TimeDecoder {
+
   /// Break time between double length standard courses (in minutes)
   static const int breakLengthInMinutes = 10;
 
@@ -41,24 +14,13 @@ final class TimeDecoder {
   /// Start time represented as TimeOfDay (7:15 AM)
   static const TimeOfDay startTime = TimeOfDay(hour: 7, minute: 15);
 
-  /// Helper function to add minutes to a TimeOfDay object
-  static TimeOfDay addMinutes(TimeOfDay time, int minutesToAdd) {
-    final totalMinutes = time.hour * 60 + time.minute + minutesToAdd;
-    final newHour = totalMinutes ~/ 60;
-    final newMinute = totalMinutes % 60;
-    return TimeOfDay(hour: newHour % 24, minute: newMinute);
-  }
-
   /// Uses start hour [code] provided by rest api
   /// to generate time in [TimeOfDay]
   static TimeOfDay decodeStartTime(int code) {
-    // Calculate the break time based on the code
     final breakTimeSumInMinutes = breakLengthInMinutes * (code ~/ 2) - (code < 2 ? 0 : breakLengthInMinutes);
-
-    // Calculate the total time to add in minutes
     final totalMinutesToAdd = (courseTimeInMinutes * (code - 1)) + breakTimeSumInMinutes;
-
-    return addMinutes(startTime, totalMinutesToAdd);
+    final totalMinutes = startTime.totalMinutes + totalMinutesToAdd;
+    return TimeOfDay(hour: totalMinutes ~/ 60 % 24, minute: totalMinutes % 60);
   }
 
   /// Uses [code] which indicates a start hour
@@ -66,14 +28,10 @@ final class TimeDecoder {
   static TimeOfDay decodeEndTime(int code, int lengthCode) {
     final sum = code + lengthCode;
     final endTime = decodeStartTime(sum);
-
-    // Subtract the break if necessary (if sum is even)
-    if (sum % 2 == 0) {
-      return addMinutes(endTime, -breakLengthInMinutes);
-    }
-
-    return endTime;
+    final totalMinutes = endTime.totalMinutes - (sum % 2 == 0 ? breakLengthInMinutes : 0);
+    return TimeOfDay(hour: totalMinutes ~/ 60 % 24, minute: totalMinutes % 60);
   }
+
 
   static int encodeStartTime(TimeOfDay timeofday)=> throw UnimplementedError("Encoding not implemented");
   static int encodeEndTime(TimeOfDay timeofday)=> throw UnimplementedError("Encoding not implemented");
