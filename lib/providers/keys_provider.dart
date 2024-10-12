2213b38e8f834dc8ae12330142c88801980f1d52
tvn24van <wojciech.smaza@gmail.com>
2024-10-13 00:42:25 +0200

    separating rest api requests from logic

--- lib/providers/keys_provider.dart
+++ lib/providers/keys_provider.dart
@@ -1,23 +1,13 @@
-import 'dart:convert';
-
 import 'package:ezak/providers/settings_provider.dart';
-import 'package:ezak/utils/constants.dart';
+import 'package:ezak/rest/rest_api.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/retry.dart';
 
 // todo add caching mechanism if possible
 final keysProvider = FutureProvider<Map<int, String>>((ref) async {
-  final teacherMode = ref.watch(SettingsProvider.instance.select((setting) => setting.isLecturer));
+  final isLecturer = ref.watch(SettingsProvider.instance.select((setting) => setting.isLecturer));
   final client = RetryClient(http.Client(), retries: 1);
-  final response = await client.get(
-    Constants.restUrl.replace(path: 'rest/${teacherMode?'prowadzacy':'wytrych'}')
-  );
-  final Iterable json = jsonDecode(utf8.decode(response.bodyBytes));
 
-  final Map<int, String> specializations = {
-    for (var i in json) i['id']: i[teacherMode? 'skrot' : 'pelna_nazwa']
-  };
-
-  return specializations;
+  return PansRestApi.fetchKeys(httpClient: client, isLecturer: isLecturer);
 });
