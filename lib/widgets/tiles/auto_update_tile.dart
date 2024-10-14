6dfe32a4a4317c2ae147639d5dd484fa6eb3e729
tvn24van <wojciech.smaza@gmail.com>
2024-10-14 14:01:37 +0200

    disable button for lecturers

--- lib/widgets/tiles/auto_update_tile.dart
+++ lib/widgets/tiles/auto_update_tile.dart
@@ -9,6 +9,7 @@ final class PansAutoUpdateTile extends ConsumerWidget{
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final autoUpdates = ref.watch(SettingsProvider.instance.select((settings) => settings.autoUpdates));
+    final isLecturer  = ref.watch(SettingsProvider.instance.select((settings) => settings.isLecturer));
 
     return ListTile(
       leading: const Icon(Icons.auto_mode_outlined),
@@ -16,7 +17,7 @@ final class PansAutoUpdateTile extends ConsumerWidget{
       subtitle: Text(L10n.of(context).auto_updates_subtitle),
       trailing: Switch.adaptive(
         value: autoUpdates,
-        onChanged:(value) {
+        onChanged: isLecturer? null : (value) {
           ref.read(SettingsProvider.instance.notifier).toggleAutoUpdates();
         },
       ),
