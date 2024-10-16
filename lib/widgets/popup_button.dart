3b6de616dc68966c4d65052ac72c3d75d912df02
tvn24van <wojciech.smaza@gmail.com>
2024-10-16 17:48:02 +0200

    add todo

--- lib/widgets/popup_button.dart
+++ lib/widgets/popup_button.dart
@@ -7,7 +7,7 @@ final class PansPopupButton extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton(
+    return PopupMenuButton( // todo use this for language selection and maybe find better alternative for this popup as it should get PopupMenuItem but instead it gets just widgets
       icon: Icon(Icons.adaptive.more),
       tooltip: MaterialLocalizations.of(context).moreButtonTooltip,
       itemBuilder: (context)=> items,
