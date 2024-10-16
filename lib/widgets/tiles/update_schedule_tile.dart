6b56d5efb4c96935b197e1ad9f60655e11b4b6a0
tvn24van <wojciech.smaza@gmail.com>
2024-10-16 17:48:50 +0200

    use method moved from removed file

--- lib/widgets/tiles/update_schedule_tile.dart
+++ lib/widgets/tiles/update_schedule_tile.dart
@@ -1,7 +1,7 @@
 import 'package:ezak/model/settings.dart';
+import 'package:ezak/pages/schedule_page.dart';
 import 'package:ezak/providers/settings_provider.dart';
 import 'package:ezak/l10n/l10n.g.dart';
-import 'package:ezak/widgets/mixins/schedule_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,7 +18,7 @@ final class PansUpdateScheduleTile extends ConsumerWidget{
       title: Text(L10n.of(context).schedule_updating),
       trailing: FilledButton(
         onPressed: isSpecializationSelected?
-          ()async=> ScheduleWidget.showUpdateDialog(context, ref):
+          ()async=> SchedulePage.showUpdateDialog(context, ref):
           null,
         child: const Icon(Icons.update)
       ),
