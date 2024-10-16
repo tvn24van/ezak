fbc53b5df69dafe5dfb3bda8c06f41e3571b5fd7
tvn24van <wojciech.smaza@gmail.com>
2024-10-16 12:25:01 +0200

    remove flutter_native_splash glue code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:ezak/providers/shared_preferences_provider.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,9 +7,6 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 import 'app.dart';
 
 void main() async{
-  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SharedPreferences.setPrefix('');
 
   usePathUrlStrategy();
@@ -22,7 +18,5 @@ void main() async{
     ],
     child: const PansApp()
   ));
-
-  FlutterNativeSplash.remove();
 
 }
