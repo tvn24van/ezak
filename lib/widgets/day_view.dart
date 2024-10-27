e78e34a3544fe4f306e0df73cc80d3733b302370
tvn24van <wojciech.smaza@gmail.com>
2024-10-27 11:13:36 +0100

    remove custom scroll behavior as now whole app supports this

--- lib/widgets/day_view.dart
+++ lib/widgets/day_view.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:ezak/model/course.dart';
 import 'package:ezak/model/decoders/time_decoder.dart';
 import 'package:ezak/utils/extensions.dart';
@@ -15,31 +13,26 @@ final class PansDayView extends StatelessWidget{
   // if date is equal to actual
   @override
   Widget build(BuildContext context) {
-    return ScrollConfiguration(
-      behavior: ScrollConfiguration.of(context).copyWith(
-        dragDevices: PointerDeviceKind.values.toSet()
-      ),
-      child: ListView(
-        physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
-        children: [
+    return ListView(
+      physics: const AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+      children: [
 
-          ...courses.asMap().entries.expand((entry) {
-            final index = entry.key;
-            final course = entry.value;
-            final int currentBreak = index>0? (course.startTime - courses[index-1].endTime).totalMinutes : TimeDecoder.breakLengthInMinutes;
+        ...courses.asMap().entries.expand((entry) {
+          final index = entry.key;
+          final course = entry.value;
+          final int currentBreak = index>0? (course.startTime - courses[index-1].endTime).totalMinutes : TimeDecoder.breakLengthInMinutes;
 
-            return [
-              if (currentBreak > TimeDecoder.breakLengthInMinutes) // todo repair this functionality
-                PansBreakIndicator(Duration(minutes: currentBreak)),
-              course.toWidget(context),
-            ];
-          }),
+          return [
+            if (currentBreak > TimeDecoder.breakLengthInMinutes) // todo repair this functionality
+              PansBreakIndicator(Duration(minutes: currentBreak)),
+            course.toWidget(context),
+          ];
+        }),
 
-          // just a little space underneath to prevent fabs from covering
-          // courses (spotted on windows, may not appear elsewhere)
-          const SizedBox(height: 90),
-        ],
-      ),
+        // just a little space underneath to prevent fabs from covering
+        // courses (spotted on windows, may not appear elsewhere)
+        const SizedBox(height: 90),
+      ],
     );
   }
 
