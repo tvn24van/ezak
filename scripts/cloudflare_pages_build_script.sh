eb9199a5b8038dcce96a749968b29e60a8f07467
tvn24van <wojciech.smaza@gmail.com>
2024-10-28 23:07:57 +0100

    add build_runner build

--- scripts/cloudflare_pages_build_script.sh
+++ scripts/cloudflare_pages_build_script.sh
@@ -7,4 +7,5 @@ else
   fi;
 #  flutter/bin/dart run flutter_launcher_icons -f flutter_launcher_icons.yaml
   flutter/bin/flutter gen-l10n
+  flutter/bin/dart run build_runner build
   flutter/bin/flutter build web --release --wasm
