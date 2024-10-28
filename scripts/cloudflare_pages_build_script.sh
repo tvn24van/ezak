d51caa79102477e80b86f28adb9108e8570eb57b
tvn24van <wojciech.smaza@gmail.com>
2024-10-28 23:44:01 +0100

    fix last lines not executing

--- scripts/cloudflare_pages_build_script.sh
+++ scripts/cloudflare_pages_build_script.sh
@@ -4,8 +4,7 @@ if [ -d "flutter" ]; then
   cd ..;
 else
   git clone https://github.com/flutter/flutter.git;
-  fi;
-#  flutter/bin/dart run flutter_launcher_icons -f flutter_launcher_icons.yaml
-  flutter/bin/flutter gen-l10n
-  flutter/bin/dart run build_runner build
+  fi &&
+  flutter/bin/flutter gen-l10n &&
+  flutter/bin/dart run build_runner build &&
   flutter/bin/flutter build web --release --wasm
