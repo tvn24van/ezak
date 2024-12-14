7c4fd823065317059a37a501c4500101ded0092d
tvn24van <wojciech.smaza@gmail.com>
2024-12-14 16:05:35 +0100

    update worker link, turn off wasm compilation as it makes drift unusable and print flutter version

--- scripts/cloudflare_pages_build_script.sh
+++ scripts/cloudflare_pages_build_script.sh
@@ -4,9 +4,12 @@ if [ -d "flutter" ]; then
   cd ..;
 else
   git clone https://github.com/flutter/flutter.git;
-  fi &&
+fi &&
+  flutter/bin/flutter config --no-analytics &&
+  flutter/bin/flutter config --no-cli-animations &&
+  flutter/bin/flutter --version &&
+  curl -L -o web/drift_worker.js https://github.com/simolus3/drift/releases/latest/download/2.22.0.js &&
+  curl -L -o web/sqlite3.wasm https://github.com/simolus3/sqlite3.dart/releases/latest/download/sqlite3.wasm &&
   flutter/bin/flutter gen-l10n &&
   flutter/bin/dart run build_runner build &&
-  curl -L -o web/drift_worker.js https://github.com/simolus3/drift/releases/latest/download/drift_worker.js &&
-  curl -L -o web/sqlite3.wasm https://github.com/simolus3/sqlite3.dart/releases/latest/download/sqlite3.wasm &&
-  flutter/bin/flutter build web --release --wasm
+  flutter/bin/flutter build web
