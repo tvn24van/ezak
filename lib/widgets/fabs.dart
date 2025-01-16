45190e6f5d0d8c55b21f92dc9bcedebe81b512da
tvn24van <wojciech.smaza@gmail.com>
2025-01-17 00:54:14 +0100

    move date button to fabs

--- lib/widgets/fabs.dart
+++ lib/widgets/fabs.dart
@@ -1,5 +1,8 @@
+import 'package:ezak/l10n/l10n.g.dart';
 import 'package:ezak/pages/schedule_page.dart';
+import 'package:ezak/providers/displayed_date_provider.dart';
 import 'package:ezak/providers/schedule_provider.dart';
+import 'package:ezak/utils/extensions.dart';
 import 'package:ezak/visuals/appereance.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,9 +16,10 @@ final class PansFloatingActionButtons extends ConsumerWidget{
     final disabled = !schedule.hasValue;
 
     return Container(
-      margin: const EdgeInsets.only(bottom: 20),
+      margin: const EdgeInsets.only(bottom: 20, left: 15, right: 15),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MediaQuery.of(context).orientation==Orientation.portrait?
+          MainAxisAlignment.spaceBetween : MainAxisAlignment.spaceEvenly,
         children: [
           FloatingActionButton(
             onPressed: disabled? null : () async{
@@ -28,6 +32,44 @@ final class PansFloatingActionButtons extends ConsumerWidget{
             tooltip: MaterialLocalizations.of(context).previousPageTooltip,
             child: Icon(Icons.adaptive.arrow_back),
           ),
+          Consumer(builder: (context, ref, child) {
+            final allAndInitialDate = ref.watch(ScheduleProvider.instance);
+            final currentDate = ref.watch(displayedDate);
+            return allAndInitialDate.when(
+              data: (data) {
+                final dates = data.dates;
+                return FloatingActionButton.extended(
+                  icon: Icon(Icons.date_range),
+                  label: Text(currentDate.toLocaleString(Localizations.localeOf(context))),
+                  tooltip: L10n.of(context).date_selection,
+                  onPressed: () async{
+                    DateTime? selectedDate = await showDatePicker(
+                      context: context,
+                      locale: Localizations.localeOf(context),
+                      keyboardType: TextInputType.datetime,
+                      helpText: L10n.of(context).choose_courses_date,
+                      initialDate: currentDate,
+                      firstDate: dates.first,
+                      lastDate: dates.last,
+                      selectableDayPredicate: (DateTime value)=> dates.contains(value)
+                    );
+                    if(selectedDate==null) return;
+                    SchedulePage.pageController?.jumpToPage(dates.indexOf(selectedDate));
+                  },
+                );
+              },
+              error: (err, stack)=> FloatingActionButton.extended(
+                onPressed: null,
+                icon: Icon(Icons.date_range),
+                label: Text(MaterialLocalizations.of(context).unspecifiedDate),
+              ),
+              loading: ()=> FloatingActionButton.extended(
+                onPressed: null,
+                icon: Icon(Icons.date_range),
+                label: CircularProgressIndicator(),
+              )
+            );
+          }),
           FloatingActionButton(
             onPressed: disabled? null : () async{
               SchedulePage.pageController?.nextPage(
