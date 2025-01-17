917c115037066f93efb991c3b1bd6eb5bc27862f
tvn24van <wojciech.smaza@gmail.com>
2025-01-17 01:08:33 +0100

    forgot 'bout links_tile.dart

---
+++ lib/widgets/tiles/drawer/links_tile.dart
@@ -0,0 +1,30 @@
+import 'package:ezak/l10n/l10n.g.dart';
+import 'package:ezak/utils/constants.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PansLinksTile extends StatelessWidget{
+  const PansLinksTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      leading: Icon(Icons.web_sharp),
+      title: Text(L10n.of(context).useful_links),
+      iconColor: IconTheme.of(context).color,
+      children: [
+        ListTile(
+          title: Text(L10n.of(context).university_website),
+          onTap: () => launchUrl(Constants.pansWebsiteUrl),
+        ),
+        if(!kIsWeb)
+          ListTile(
+            title: Text(L10n.of(context).web_app),
+            onTap: () => launchUrl(Constants.webAppUrl),
+          )
+      ],
+    );
+  }
+
+}
