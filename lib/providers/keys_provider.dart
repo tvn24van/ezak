f88c3ba37722ef081606275f6c134748ab8463fb
tvn24van <wojciech.smaza@gmail.com>
2025-09-22 23:26:04 +0200

    use modern way to access provide modifier

--- lib/providers/keys_provider.dart
+++ lib/providers/keys_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/retry.dart';
 
-final keysProvider = FutureProviderFamily<Map<int, String>, bool>((ref, isLecturer) async {
+final keysProvider = FutureProvider.family<Map<int, String>, bool>((ref, isLecturer) async {
   final client = RetryClient(http.Client(), retries: 1);
   final keys = PansRestApi.fetchKeys(httpClient: client, isLecturer: isLecturer);
   client.close();
