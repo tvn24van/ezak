812b093489651d8a73857587b9fb1631892cb2ba
tvn24van <wojciech.smaza@gmail.com>
2025-10-08 02:11:56 +0200

    load deferred component just once

--- lib/utils/deferred_page_builder.dart
+++ lib/utils/deferred_page_builder.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 
-class DeferredPageBuilder extends StatelessWidget{
+class DeferredPageBuilder extends StatefulWidget{
   final Future<dynamic> Function() future;
   final Widget Function() page;
 
   const DeferredPageBuilder({super.key, required this.future, required this.page});
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: future(),
-      builder: (context, snapshot) => snapshot.connectionState==ConnectionState.done? page() : Scaffold()
-    );
+  State<StatefulWidget> createState() {
+    return _DeferredPageBuilderState();
   }
 
+}
+
+class _DeferredPageBuilderState extends State<DeferredPageBuilder>{
+  late final _loadedFuture = widget.future();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _loadedFuture,
+      builder: (context, snapshot) => snapshot.connectionState==ConnectionState.done? widget.page() : Scaffold()
+    );
+  }
 }
