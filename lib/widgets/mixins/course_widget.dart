401e8a305e1881a6b72b4df39b5be278e873df50
tvn24van <wojciech.smaza@gmail.com>
2025-10-24 12:24:30 +0200

    use Card.filled instead to simplify widget tree

--- lib/widgets/mixins/course_widget.dart
+++ lib/widgets/mixins/course_widget.dart
@@ -6,30 +6,24 @@ import 'package:flutter/material.dart';
 
 mixin CourseWidget on CourseModel implements WidgetTransformable{
 
-  static const BorderRadius _borderRadius = BorderRadius.all(Radius.circular(16));
-
   @override
   Widget toWidget(BuildContext context){
-    return Container(
+    return Card.filled(
       margin: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+      clipBehavior: Clip.hardEdge,
+      color: Color.lerp(Theme.of(context).scaffoldBackgroundColor, group.color, .2),
       child: InkWell(
-        borderRadius: _borderRadius,
         onTap: ()async=> {},
         //   Navigator.push(
         //     context,
         //     MaterialPageRoute(builder: (context) => CourseHero(course: this))
         //   ),
-        child: Ink(
-          padding: const EdgeInsets.all(15),
-          decoration: BoxDecoration(
-            color: Color.lerp(Theme.of(context).scaffoldBackgroundColor, group.color, .2),
-            borderRadius: _borderRadius
-          ),
+
+        child: Padding(
+          padding: EdgeInsets.all(15),
           child: Row(
             children: [
               Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(L10n.of(context).from_hour(startTime.format(context))),
                   Text(L10n.of(context).to_hour(endTime.format(context))),
