97602c5c788e3638731ce33bdc6e96bd1f160af6
tvn24van <wojciech.smaza@gmail.com>
2025-11-15 01:21:39 +0100

    use Future as return type

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async{
   SharedPreferences.setPrefix('');
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
-  useLocalCert();
+  await useLocalCert();
   runApp( ProviderScope(
     overrides: [
       sharedPreferences.overrideWithValue(
--- lib/utils/local_cert.dart
+++ lib/utils/local_cert.dart
@@ -4,7 +4,7 @@ import 'package:ezak/utils/extensions.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
-void useLocalCert() async{
+Future<void> useLocalCert() async{
   if(defaultTargetPlatform.isMobile()) {
     final data = await rootBundle.load("assets/intermediate_ca.crt");
     SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
