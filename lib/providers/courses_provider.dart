62f0b70ca2224c8877421c3c8055baf236d043d5
tvn24van <wojciech.smaza@gmail.com>
2026-03-06 00:44:31 +0100

    convert to stream providers

--- lib/providers/courses_provider.dart
+++ lib/providers/courses_provider.dart
@@ -1,63 +1,15 @@
-import 'dart:async';
-
 import 'package:ezak/db/cache_db.dart';
 import 'package:ezak/model/course.dart';
-import 'package:ezak/providers/dates_provider.dart';
-import 'package:ezak/providers/initial_date_provider.dart';
-import 'package:ezak/providers/max_groups_provider.dart';
 import 'package:ezak/providers/settings_provider.dart';
-import 'package:flutter/material.dart';
+import 'package:ezak/utils/extensions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-typedef LoadedCourses = Map<DateTime, List<Course>>;
+final coursesProvider = StreamProvider.autoDispose.family<List<Course>, DateTime>((ref, date) async* {
+  final db = ref.watch(CacheDb.instance);
+  final key = ref.watch(SettingsProvider.key);
+  final isLecturer = ref.watch(SettingsProvider.isLecturer);
+  final groups = await ref.watch(SettingsProvider.groupsInfluencingCurrentSchedule.future);
+  ref.cacheFor(const Duration(minutes: 1));
 
-class CoursesProvider extends AsyncNotifier<LoadedCourses>{
-  static final instance = AsyncNotifierProvider<CoursesProvider, LoadedCourses>(CoursesProvider.new);
-
-  @override
-  Future<Map<DateTime, List<Course>>> build() async {
-    final db = ref.watch(CacheDb.instance);
-    final key = ref.watch(SettingsProvider.key);
-    final isLecturer = ref.watch(SettingsProvider.isLecturer);
-
-    final maxGroups = await ref.watch(maxGroupsProvider.future);
-    final groups = {... ref.watch(SettingsProvider.groups)}..removeWhere((key, value) => !maxGroups.keys.contains(key));
-    final initialDate = await ref.watch(initialDateProvider.future);
-    final datesToLoad = await getDatesAround(initialDate);
-
-    return db.getCourses(key: key, isLecturer: isLecturer, groups: groups, dates: datesToLoad);
-  }
-
-  Future<void> loadCourses(DateTime date) async {
-    final db = ref.read(CacheDb.instance);
-    final key = ref.read(SettingsProvider.key);
-    final isLecturer = ref.read(SettingsProvider.instance.select((value) => value.isLecturer));
-
-    final maxGroups = await ref.read(maxGroupsProvider.future);
-    // filter schedule only by groups it contains
-    final groups = {... ref.read(SettingsProvider.groups)}..removeWhere((key, value) => !maxGroups.keys.contains(key));
-    final datesToLoad = (await getDatesAround(date))
-        .where((d) => !state.value!.keys.contains(d))
-        .toList();
-
-    if(datesToLoad.isEmpty) return;
-
-    final courses = await db.getCourses(key: key, isLecturer: isLecturer, groups: groups, dates: datesToLoad);
-    debugPrint("Loading courses for $datesToLoad");
-
-    state = AsyncValue.data(
-        {...state.value!}..addAll(courses)
-    );
-  }
-
-  Future<List<DateTime>> getDatesAround(DateTime date) async{ //it doesn't fit here
-    final allDates = await ref.read(datesProvider.future);
-    final dateIndex = allDates.indexOf(date);
-    final indexes = [dateIndex-1, dateIndex, dateIndex+1].where((e) => e>=0 && e<allDates.length);
-    return allDates.asMap().entries
-        .where((e) => indexes.contains(e.key))
-        .map((e) => e.value)
-        .toList();
-  }
-
-}
+  yield* db.getCoursesStream(key: key, isLecturer: isLecturer, groups: groups, date: date);
+});
--- lib/providers/dates_provider.dart
+++ lib/providers/dates_provider.dart
@@ -2,11 +2,11 @@ import 'package:ezak/db/cache_db.dart';
 import 'package:ezak/providers/settings_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final datesProvider = FutureProvider<List<DateTime>>((ref) async {
+final datesProvider = StreamProvider<List<DateTime>>((ref) {
   final db = ref.watch(CacheDb.instance);
   final isLecturer = ref.watch(SettingsProvider.isLecturer);
   final key = ref.watch(SettingsProvider.key);
   final groups = ref.watch(SettingsProvider.groups);
 
-  return db.getDates(key: key, isLecturer: isLecturer, groups: groups);
+  return db.getDatesStream(key: key, isLecturer: isLecturer, groups: groups);
 });
