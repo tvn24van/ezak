2b1f193c3a64aef1706fc124eec07e1dafbd4e25
tvn24van <wojciech.smaza@gmail.com>
2026-03-06 00:43:30 +0100

    use clientProvider

--- lib/providers/keys_provider.dart
+++ lib/providers/keys_provider.dart
@@ -1,11 +1,9 @@
+import 'package:ezak/providers/http_client_provider.dart';
 import 'package:ezak/rest/rest_api.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
-import 'package:http/retry.dart';
 
 final keysProvider = FutureProvider.family<Map<int, String>, bool>((ref, isLecturer) async {
-  final client = RetryClient(http.Client(), retries: 1);
+  final client = ref.watch(clientProvider(null));
   final keys = await PansRestApi.fetchKeys(httpClient: client, isLecturer: isLecturer);
-  client.close();
   return keys;
 });
