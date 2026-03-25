3a6a92228bd44386bb887993e408162e65270f71
tvn24van <wojciech.smaza@gmail.com>
2026-03-25 02:27:27 +0100

    add methods for manipulating currently displayed date

--- lib/providers/displayed_date_provider.dart
+++ lib/providers/displayed_date_provider.dart
@@ -1,10 +1,11 @@
+import 'package:ezak/providers/dates_provider.dart';
 import 'package:ezak/providers/initial_date_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //todo add checking if was current day, but should be now changed
 
 class DisplayedDateProvider extends AsyncNotifier<DateTime>{
-  static final instance = AsyncNotifierProvider<DisplayedDateProvider, DateTime>(DisplayedDateProvider.new);
+  static final instance = AsyncNotifierProvider(DisplayedDateProvider.new);
   @override
   Future<DateTime> build() async{
     return ref.watch(initialDateProvider.future);
@@ -12,6 +13,22 @@ class DisplayedDateProvider extends AsyncNotifier<DateTime>{
 
   void change(DateTime newDate){
     state = AsyncValue.data(newDate);
+  }
+
+  Future next() async{
+    final dates = await ref.read(datesProvider.future);
+    final current = state.value;
+    if(current!=null && current!=dates.last){
+      state = AsyncValue.data(dates.firstWhere((element) => element.isAfter(current)));
+    }
+  }
+
+  Future previous() async{
+    final dates = await ref.read(datesProvider.future);
+    final current = state.value;
+    if(current!=null && current!=dates.first){
+      state = AsyncValue.data(dates.lastWhere((element) => element.isBefore(current)));
+    }
   }
 
 }
