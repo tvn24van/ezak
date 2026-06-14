8d02a875a59b54a4773e563f7404e73db165ff04
tvn24van <wojciech.smaza@gmail.com>
2026-06-15 00:47:01 +0200

    use spacing instead of SizedBox

--- lib/widgets/info_button.dart
+++ lib/widgets/info_button.dart
@@ -17,61 +17,57 @@ final class PansInfoButton extends ConsumerWidget{
         showDialog(context: context, builder: (context) {
           return AlertDialog(
             title: Text(L10n.of(context).informations),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
+            content: Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
               children: [
-                Wrap(
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: Group.values.map((group) {
-                        return Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                color: highContrast?
-                                  group.color.ensureWcagAaContrast(context):
-                                  Color.lerp(Theme.of(context).scaffoldBackgroundColor, group.color, .5),
-                                shape: BoxShape.circle
-                              ),
-                            ),
-                            const SizedBox(width: 10, height: 30,),
-                            Text(L10n.of(context).group_name(group.name))
-                          ],
-                        );
-                      }).toList(),
-                    ),
-                    const VerticalDivider(),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 10,
+                  children: Group.values.map((group) {
+                    return Row(
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 10,
                       children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(Icons.home_work),
-                            const SizedBox(width: 10, height: 30,),
-                            Text(L10n.of(context).full_time_course)
-                          ],
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: highContrast?
+                              group.color.ensureWcagAaContrast(context):
+                              Color.lerp(Theme.of(context).scaffoldBackgroundColor, group.color, .5),
+                            shape: BoxShape.circle
+                          ),
                         ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(Icons.computer),
-                            const SizedBox(width: 10, height: 30,),
-                            Text(L10n.of(context).online_course)
-                          ],
-                        ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(Icons.share_arrival_time_outlined),
-                            const SizedBox(width: 10, height: 30,),
-                            Text(L10n.of(context).longer_break)
-                          ],
-                        )
+                        Text(L10n.of(context).group_name(group.name))
+                      ],
+                    );
+                  }).toList(),
+                ),
+                const VerticalDivider(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 10,
+                      children: [
+                        const Icon(Icons.home_work),
+                        Text(L10n.of(context).full_time_course)
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 10,
+                      children: [
+                        const Icon(Icons.computer),
+                        Text(L10n.of(context).online_course)
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 10,
+                      children: [
+                        const Icon(Icons.share_arrival_time_outlined),
+                        Text(L10n.of(context).longer_break)
                       ],
                     )
                   ],
