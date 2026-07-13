41407ed82a28ca0f45dafbc4948c702787922682
tvn24van <wojciech.smaza@gmail.com>
2026-07-13 20:07:58 +0200

    use generic app store link

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -13,7 +13,7 @@ final class Constants{
 
   static final googlePlayUrl = Uri.https('play.google.com', '/store/apps/details', {'id': 'pl.tvn24van.ezak'});
 
-  static final appStoreUrl = Uri.https('apps.apple.com', '/pl/app/id6788749883');
+  static final appStoreUrl = Uri.https('apps.apple.com', '/app/id6788749883');
   
   static final videoClip = Uri.https('youtube.com', '/watch', {'v': 'zYQOpCfCOxI'});
 
